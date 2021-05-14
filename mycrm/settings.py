5c49bdf7dfd1b5d3f45efcb168e67ec855889245
yrasiq <83963015+yrasiq@users.noreply.github.com>
2021-05-14 21:00:58 +0300

    get DEBUG settings from .env

--- mycrm/settings.py
+++ mycrm/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = env('DEBUG')
-DEBUG = True
+DEBUG = env('DEBUG')
 
 SECURE_SSL_REDIRECT = False
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
